2865b20c5b77e810fe3db83ef275e6742f947b25
kishore <kishorevaddi1204@gmail.com>
2026-07-19 14:10:16 +0530

    external website added

--- src/app/world/world.component.ts
+++ src/app/world/world.component.ts
@@ -17,10 +17,10 @@ export class WorldComponent {
       link: 'https://github.com/KISHOREVADDI'
     },
     {
-      name: 'LeetCode',
-      icon: 'fa-solid fa-code',
-      description: 'Algorithm challenges and solutions',
-      link: 'https://leetcode.com/u/Kishore-1204/'
+      name: 'TryHackMe',
+      icon: 'fa-solid fa-shield-halved',
+      description: 'Cybersecurity learning platform',
+      link: 'https://kishorevaddi.github.io/tryhackmedashboard/'
     },
     {
       name: 'LinkedIn',
@@ -28,6 +28,13 @@ export class WorldComponent {
       description: 'Professional network and connections',
       link: 'https://www.linkedin.com/in/kishorevaddi'
     },
+    {
+      name: 'LeetCode',
+      icon: 'fa-solid fa-code',
+      description: 'Algorithm challenges and solutions',
+      link: 'https://leetcode.com/u/Kishore-1204/'
+    },
+
     {
       name: 'CodeChef',
       icon: 'fa-solid fa-laptop-code',
@@ -39,12 +46,7 @@ export class WorldComponent {
       icon: 'fa-brands fa-hackerrank',
       description: 'Coding challenges and skills',
       link: 'https://www.hackerrank.com/profile/kishorevaddi1204'
-    },
-    {
-      name: 'TryHackMe',
-      icon: 'fa-solid fa-shield-halved',
-      description: 'Cybersecurity learning platform',
-      link: 'https://tryhackme.com/p/kishore1204'
     }
+
   ];
 }
